eab94e3e2f89ad08e5a66e46fa9e59a2691e6507
James Bremner <james@ravenspoint.com>
2021-04-12 15:54:19 -0400

    remove dead code

--- src/comExplorer.cpp
+++ src/comExplorer.cpp
@@ -26,10 +26,6 @@ cGUI::cGUI()
     myForm.move( 50,50,450,400);
     myForm.text("COM Explorer");
 
-//    myPortlb.move(20,20,100,30);
-//    myPortlb.text("COM Port");
-//    myPorteb.move(140,20,50,30);
-//    myPorteb.text("9");
     myConnectbn.move( 210, 20, 100,30);
     myConnectbn.bgcolor(0x9090FF);
     myConnectbn.text("Connect");
@@ -59,34 +55,6 @@ cGUI::cGUI()
         }
 
     });
-
-//    myForm.events().asyncReadComplete([&]( int id )
-//    {
-//        // packet has been received
-//
-//        auto r = myTalker.readData();
-//        myRcvList.add( "read " + std::to_string( r.size() ) + " bytes");
-//        std::stringstream ss;
-//        ss << std::hex;
-//        int k = 0;
-//        for( auto c : r )
-//        {
-//            ss << "0x" << (int)c << " ";
-//            k++;
-//            if( k > 9 )
-//            {
-//                myRcvList.add( ss.str() );
-//                ss.str("");
-//                k = 0;
-//            }
-//        }
-//        myRcvList.add( ss.str() );
-//        myRcvList.update();
-//
-//        // wait for next
-//
-//        myTalker.read_async( -1 );
-//    });
 
     myForm.show();
 }
